13f54179219f86c71140f7eceb482eb83e79b595
reinheimer1 <thorsten.reinheimer@hcl.com>
2024-11-13 16:02:53 +0100

    fixed missing commands in create_certificates.sh script

--- ssl/create_certificates.sh
+++ ssl/create_certificates.sh
@@ -1,1 +1,4 @@
-{\rtf1}
+openssl genrsa -out tls.key 2048
+openssl req -new -key tls.key -out tls.csr
+openssl x509 -req -days 358000 -in tls.csr -signkey tls.key -out tls.crt
+cat tls.key tls.crt > localhost.pem
